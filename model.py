e50811614076e7e67fdd2767c63267233aed5adf
WhatWeEat <1053246332@qq.com>
2024-06-14 20:46:19 +0800

    model写好了用dpm生成

--- model.py
+++ model.py
@@ -1,21 +1,32 @@
 import torch
 import numpy as np
+from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 from segment_anything import sam_model_registry, SamPredictor
+from diffusers import StableDiffusionInpaintPipeline, DPMSolverMultistepScheduler
+from loguru import logger
 model_type="vit_h"
 sam_checkpoint="./models/sam_vit_h_4b8939.pth"
 sam = None
 try:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint).to(device)
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint).to("cuda:0")
     predictor = SamPredictor(sam)
-    print(f"loading sucessfully.....")
+    logger.info(f"loading sam sucessfully.....")
+
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        "models/stable-diffusion",
+        torch_dtype=torch.float16,
+    )
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    pipe.to("cuda:1")
+    logger.info(f"loading stable diffusion sucessfully")
+
 except Exception as e:
     print(f"Error loading model: {e}")
 
 
-def segment_image(image_path, points):
+def segment_image(image, points):
     """
     使用 SAM 模型对图像进行分割。
 
@@ -29,15 +40,6 @@ def segment_image(image_path, points):
     np.ndarray: 分割后的图像和掩码。
     """
 
-
-    try:
-        image = cv2.imread(image_path)
-        if image is None:
-            raise FileNotFoundError(f"Image file {image_path} not found.")
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    except Exception as e:
-        print(f"Error loading image: {e}")
-        return None, None
 
     try:
         predictor.set_image(image)
@@ -54,6 +56,7 @@ def segment_image(image_path, points):
         best_mask = masks[np.argmax(scores)]
 
         segmented_image = image.copy()
+        # 0为没有被sam出来的部分， 1为sam出来的
         segmented_image[best_mask == 0] = 0
 
         return segmented_image, best_mask
@@ -61,17 +64,38 @@ def segment_image(image_path, points):
         print(f"Error during prediction: {e}")
         return None, None
 
+def generate_image(image, mask_image):
+    prompt = "high resolution"
+    image_pil = Image.fromarray(image)
+    inverted_mask_image = (mask_image.astype(np.uint8))
+
+    image = pipe(prompt="", image=image_pil, mask_image=inverted_mask_image).images[0]
+    
+    return image
+
 if __name__ == "__main__":
     image_path = "./image.png"
+    image = None
+    try:
+        image = cv2.imread(image_path)
+        if image is None:
+            raise FileNotFoundError(f"Image file {image_path} not found.")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    except Exception as e:
+        print(f"Error loading image: {e}")
+
     points = [(320, 228)]
 
-    segmented_image,best_mask = segment_image(image_path, points)
+    segmented_image,best_mask = segment_image(image, points)
 
     plt.imshow(segmented_image)
     plt.title("Segmented Image")
     plt.savefig('1.png')
 
     plt.imshow(best_mask)
-    plt.title("Segmented Image")
+    plt.title("Segmented Mask Image")
     plt.savefig('1_mask.png')
+    image1 = generate_image(image, best_mask)
+    image1.save("./yellow_cat_on_park_bench1.png")
+    image.save("./origin.png")
     # plt.show()
