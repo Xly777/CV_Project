390cf61380fe126e464575dee76921933cfd2fe5
Xerry <1435627567@qq.com>
2024-06-14 21:02:25 +0800

    完成前后端连接

--- frontend.py
+++ frontend.py
@@ -1,11 +1,6 @@
 import gradio as gr
 from PIL import Image
-
-def process_image(image, coordinates):
-    # 这个函数将由你来实现
-    # 例如：result_image = your_function(image, coordinates)
-    result_image = image  # 暂时返回原图，之后你可以替换成你的函数逻辑
-    return result_image
+from model import segment_image,generate_image
 
 # 存储点击位置的列表
 click_coordinates = []
@@ -13,22 +8,30 @@ click_coordinates = []
 def get_coordinates(img, evt: gr.SelectData):
     global click_coordinates
     click_coordinates.append((evt.index[0], evt.index[1]))
-    return img, click_coordinates, process_image(img, click_coordinates)
+    segmented_image, best_mask=segment_image(img, click_coordinates)
+    # 将mask图像的透明度设置为50%
+    mask_image = best_mask.convert("RGBA")
+    alpha = mask_image.split()[3]
+    alpha = alpha.point(lambda p: p * 0.5)
+    mask_image.putalpha(alpha)
+    # 将mask图像覆盖到原图上
+    combined_image = Image.alpha_composite(img.convert("RGBA"), mask_image)
+    return img, click_coordinates, combined_image,best_mask
 
 def clear_coordinates():
     global click_coordinates
     click_coordinates = []
     return ""
-
+best_mask=None
 with gr.Blocks() as demo:
     with gr.Row():
         img_input = gr.Image(type="pil", label="上传图片")
-        img_output = gr.Image(type="pil", label="处理后图片")
-        
+        img_mask = gr.Image(type="pil", label="处理后图片")
+        img_result=gr.Image(type="pil", label="结果图片")
     coordinates_display = gr.Textbox(label="点击位置坐标", interactive=False)
     clear_button = gr.Button("清除点击")
-
-    img_input.select(get_coordinates, [img_input], [img_input, coordinates_display, img_output])
+    result_button=gr.Button("生成图片")
+    img_input.select(get_coordinates, [img_input], [img_input, coordinates_display, img_mask,best_mask])
     clear_button.click(clear_coordinates, [], coordinates_display)
-
+    result_button.click(generate_image, [img_input,best_mask], img_result)
 demo.launch()
