f2bff6d00ce27a42c29b851efc79214f89a0b3d1
WhatWeEat <1053246332@qq.com>
2024-06-14 21:46:55 +0800

    改好了数据类型不一致的问题

--- frontend.py
+++ frontend.py
@@ -1,16 +1,19 @@
 import gradio as gr
 from PIL import Image
 from model import segment_image,generate_image
-
+import numpy as np
 # 存储点击位置的列表
 click_coordinates = []
+def generate_gradio_image(image_input,best_mask):
+    return generate_image(np.array(image_input),best_mask)
 
 def get_coordinates(img, evt: gr.SelectData):
     global click_coordinates
     click_coordinates.append((evt.index[0], evt.index[1]))
-    segmented_image, best_mask=segment_image(img, click_coordinates)
+    segmented_image, best_mask=segment_image(np.array(img), click_coordinates)
     # 将mask图像的透明度设置为50%
-    mask_image = best_mask.convert("RGBA")
+    _best_mask=Image.fromarray(best_mask)
+    mask_image = _best_mask.convert("RGBA")
     alpha = mask_image.split()[3]
     alpha = alpha.point(lambda p: p * 0.5)
     mask_image.putalpha(alpha)
@@ -22,8 +25,9 @@ def clear_coordinates():
     global click_coordinates
     click_coordinates = []
     return ""
-best_mask=None
+
 with gr.Blocks() as demo:
+    best_mask=gr.State(None)
     with gr.Row():
         img_input = gr.Image(type="pil", label="上传图片")
         img_mask = gr.Image(type="pil", label="处理后图片")
@@ -33,5 +37,6 @@ with gr.Blocks() as demo:
     result_button=gr.Button("生成图片")
     img_input.select(get_coordinates, [img_input], [img_input, coordinates_display, img_mask,best_mask])
     clear_button.click(clear_coordinates, [], coordinates_display)
-    result_button.click(generate_image, [img_input,best_mask], img_result)
-demo.launch()
+    result_button.click(generate_gradio_image, [img_input,best_mask], img_result)
+
+demo.launch(server_name="0.0.0.0", server_port=7860)
