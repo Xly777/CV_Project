816b2d1c46e3d418508e0882b1005a42f8c48405
Xerry <1435627567@qq.com>
2024-06-15 12:58:17 +0800

    增加坐标可视化

--- frontend.py
+++ frontend.py
@@ -6,7 +6,7 @@ import numpy as np
 click_coordinates = []
 def generate_gradio_image(image_input,best_mask):
     return generate_image(np.array(image_input),best_mask)
-def plot_coordinates_on_image(img, coords, point_color=(0, 0, 255), point_radius=5):
+def plot_coordinates_on_image(img, coords, point_color=(0, 139, 139), point_radius=15):
     draw = ImageDraw.Draw(img)
     # 绘制坐标点
     for x, y in coords:
@@ -24,9 +24,8 @@ def get_coordinates(img, evt: gr.SelectData):
     mask_image.putalpha(alpha)
     # 将mask图像覆盖到原图上
     combined_image = Image.alpha_composite(img.convert("RGBA"), mask_image)
-    image_input2=img
-    image_input2=plot_coordinates_on_image(image_input2, click_coordinates)
-    return img, click_coordinates, combined_image,best_mask,image_input2
+    combined_image=plot_coordinates_on_image(combined_image, click_coordinates)
+    return img, click_coordinates, combined_image,best_mask
 
 def clear_coordinates():
     global click_coordinates
@@ -40,14 +39,13 @@ with gr.Blocks() as demo:
     best_mask=gr.State(None)
     with gr.Row():
         img_input = gr.Image(type="pil", label="上传图片")
-        img_input2=gr.Image(type="pil", label="坐标可视化")
         img_mask = gr.Image(type="pil", label="处理后图片")
         img_result=gr.Image(type="pil", label="结果图片")
     coordinates_display = gr.Textbox(label="点击位置坐标", interactive=False)
     cancel_button=gr.Button("撤销上一次点击")
     clear_button = gr.Button("清除所有点击")
     result_button=gr.Button("生成图片")
-    img_input.select(get_coordinates, [img_input], [img_input, coordinates_display, img_mask,best_mask,img_input2])
+    img_input.select(get_coordinates, [img_input], [img_input, coordinates_display, img_mask,best_mask])
     cancel_button.click(cancel, [], coordinates_display)
     clear_button.click(clear_coordinates, [], coordinates_display)
     result_button.click(generate_gradio_image, [img_input,best_mask], img_result)
